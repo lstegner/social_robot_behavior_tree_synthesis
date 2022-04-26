3fdf1ed892c85aca01c4b1511a01065e585dcbb3
lstegner <stegner@wisc.edu>
2022-04-26 00:32:46 -0500

    bug fixes

--- store.py
+++ store.py
@@ -1,70 +1,27 @@
 #!/usr/bin/env python3
 
-class WorldStates:
-    def __init__(self):
-        # # World States
-        # self.humanReady = False
-        # self.greetOccurred = False
-
-        # # HumanJustSpoke
-        # self.humanAffirm = False
-        # self.humanDeny = False
-        # self.humanAskInfo = False
-        # self.humanAskDirections = False
-        # self.humanGoodbye = False
-        # self.humanNotUnderstood = False
-
-        # # RobotJustActed
-        # self.robotPrompt = False
-        # self.robotValidate = False
-        # self.robotListen = False
-        # self.robotAnswerInfo = False
-        # self.robotAnswerDirections = False
-        # self.robotAskToRepeat = False
-        # self.robotHelp = False
-        # self.robotFarewell = False
-
-        self.state_list = ["humanReady","greetOccurred",
-        "humanAffirm","humanDeny","humanAskInfo","humanAskDirections","humanGoodbye","humanNotUnderstood",
-        "robotPrompt","robotValidate","robotListen","robotAnswerInfo","robotAnswerDirections","robotAskToRepeat","robotHelp","robotFarewell"]
-
-        self.humanJustSpoke = ["humanAffirm","humanDeny","humanAskInfo","humanAskDirections","humanGoodbye","humanNotUnderstood"]
-        self.robotJustActed = ["robotPrompt","robotValidate","robotListen","robotAnswerInfo","robotAnswerDirections","robotAskToRepeat","robotHelp","robotFarewell"]
-
-    # def HumanJustSpoke(self):
-    #     if self.humanAffirm or self.humanDeny or self.humanAskInfo or self.humanAskDirections or self.humanGoodbye or self.humanNotUnderstood:
-    #         return True
-    #     else:
-    #         return False
-
-    # def RobotJustActed(self):
-    #     if self.robotPrompt or self.robotValidate or self.robotListen or self.robotAnswerInfo or self.robotAnswerDirections or self.robotAskToRepeat or self.robotHelp or self.robotFarewell:
-    #         return True
-    #     else:
-    #         return False
-
-
 class Behavior:
     def __init__(self, n="",precons=[],postcons=[]):
         self.name = n
         self.preconditions = precons
         self.postconditions = postcons
 
-    # def get_preconditions(self):
-    #     precons = []
-    #     for precon in self.preconditions:
-    #         precons.append(precon[0])
-
-    #     return precons
-
-    # def get_postconditions(self):
-    #     postcons = []
-    #     for postcon in self.postconditions:
-    #         postcons.append(postcon[0])
-
-    #     return postcons
-
 class BehaviorBank:
+    def __init__(self):
+        self.behaviors = []
+        self.behaviors.append(Behavior("Greet",{"humanReady":True,"greetOccurred":False},[{"greetOccurred":True}]))
+        self.behaviors.append(Behavior("Announce",{"humanReady":False},[{"humanReady":True},{"humanReady":False}]))
+        self.behaviors.append(Behavior("OfferHelp",{"humanReady":True,"greetOccurred":True},[{"offeredHelp":True}]))
+
+        
+
+    def get_behavior(self, name):
+        for behavior in self.behaviors:
+            if behavior.name == name:
+                return behavior
+
+
+class BehaviorBank_backup:
     def __init__(self):
         self.behaviors = []
         self.behaviors.append(Behavior("Greet",{"humanReady":True,"greetOccurred":False},[{"greetOccurred":True}]))
@@ -83,7 +40,6 @@ class BehaviorBank:
 
         #self.behaviors.append(Behavior("Point",{"humanAskDirections":True},[{"robotDirections":True}]))
         #self.behaviors.append(Behavior("Announce",{"humanReady":False},[{"humanReady":True},{"humanReady":False}]))
-
 
 
 
--- synthesis.py
+++ synthesis.py
@@ -1,5 +1,5 @@
 from anytree import NodeMixin, RenderTree
-from store import WorldStates, Behavior, BehaviorBank
+from store import BehaviorBank
 
 class BT_Node(NodeMixin):
     def __init__(self, name, type, parent=None, children=None):
@@ -25,40 +25,49 @@ class Synthesis:
         self.behavior_bank = behavior_bank
 
     def find_child_action(self, goal_state, parent, curr_depth, max_depth):
+        # if there are multiple goal states, we want them to be joined by a sequence node
+        goal_sequence = None
+        if len(goal_state) > 1:
+            goal_sequence = BT_Node(curr_depth,"Sequence (for multiple goals)",parent)
+
         # for each var in goal_state:
-        print(goal_state)
         for goal_condition in goal_state:
             # find action that results in var
             action_to_add = self.find_correct_action(goal_condition)
 
             if action_to_add is not None:
-                # add action to behavior tree
+                # add stuff to behavior tree
 
                 # replace condition with selector node. condition is left child, action is right child
-                new_selector = BT_Node(curr_depth,"Selector",parent)
-                conditional = BT_Node(curr_depth+1,"Condition: " + str(goal_condition),new_selector)
-                new_sequence = BT_Node(curr_depth+1,"Sequence",new_selector)
+                new_selector = BT_Node(curr_depth+1,"Selector",parent)
+                update_curr_depth = curr_depth
+                if goal_sequence is not None:
+                    new_selector.parent = goal_sequence
+                    update_curr_depth = curr_depth + 1
+                else:
+                    new_selector.parent = parent
+                conditional = BT_Node(update_curr_depth+2,"Condition: " + str(goal_condition),new_selector)
+                new_sequence = BT_Node(update_curr_depth+2,"Sequence",new_selector)
 
                 # if action has pre-conditions, 
                 # right child should be sequence node with each pre-condition as child and action as right-most child
                 if len(list(self.behavior_bank.get_behavior(action_to_add).preconditions)) > 0:
                     for item in self.behavior_bank.get_behavior(action_to_add).preconditions:
                         x = {item: self.behavior_bank.get_behavior(action_to_add).preconditions[item]}
-                        if(curr_depth < max_depth):
-                            self.find_child_action([x], new_sequence, curr_depth+2, max_depth)
+                        if(update_curr_depth < max_depth):
+                            self.find_child_action([x], new_sequence, update_curr_depth+2, max_depth)
                         else:
-                            new_precon = BT_Node(curr_depth+2,str(x), new_sequence)
+                            new_precon = BT_Node(update_curr_depth+3,str(x), new_sequence)
 
-                # if action has multiple possible post-conditions, discard non-desirable ones and add as a child: 
+                # if action has multiple possible post-conditions, discard non-desirable ones and add as a child: 22
                 # sequence node, with right child as condition (not desired result) and left child as (offer help)
                 if len(self.behavior_bank.get_behavior(action_to_add).postconditions) > 1:
-                    extra_responses = BT_Node(curr_depth+2, "Error, undesirable input", new_sequence)                   
+                    unexpected_input_seq = BT_Node(update_curr_depth+3, "Potential undesirable input", new_sequence)
+                    extra_response_check = BT_Node(update_curr_depth+4, "Check other post conditions", unexpected_input_seq)
+                    extra_responses = BT_Node(update_curr_depth+4, "Offer help", unexpected_input_seq)                   
 
                 # add action as last child
-                action_node = BT_Node(curr_depth+2,action_to_add,new_sequence)
-
-            # check if initial_state has been met OR the depth limit is reached
-            # otherwise, for each pre-condition, call find_child_action(goal_state)
+                action_node = BT_Node(update_curr_depth+3,action_to_add,new_sequence)
 
             # return the behavior tree created
         return parent
@@ -82,9 +91,9 @@ class Synthesis:
 
 if __name__ == '__main__':
     # set the goal
-    goal = [{"greetOccurred":True},{"END":True}]
+    goal = [{"offeredHelp":True}]
 
-    # initialize the behaviors
+    # initialize the behaviorsfind_child_action
     behavior_bank = BehaviorBank()
 
     # initialize BT
@@ -94,8 +103,8 @@ if __name__ == '__main__':
     synthesizer = Synthesis(goal, behavior_bank)
 
     # run
-    tree = synthesizer.find_child_action(goal, root, curr_depth=0, max_depth=5)
+    tree = synthesizer.find_child_action(goal, root, curr_depth=0, max_depth=3)
     print("------------final tree------------")
     tree.print(root)
-    tree.check(root)
+    #tree.check(root)
 
